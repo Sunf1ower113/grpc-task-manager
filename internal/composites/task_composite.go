ad2dc94aa497c3aa43188a9b0ede9d89b4f42eb0
Semyon <sonnenblumenglas@gmail.com>
2024-11-26 21:31:12 +0500

    Implement TaskService with business logic and repository integration & Add mock repository for TaskService unit testing

--- internal/composites/task_composite.go
+++ internal/composites/task_composite.go
@@ -1,17 +1,23 @@
 package composites
 
 import (
+	"database/sql"
+	storage "github.com/Sunf1ower113/grpc-task-manager/internal/adapters/db"
 	"github.com/Sunf1ower113/grpc-task-manager/internal/domain/repository"
+	"github.com/Sunf1ower113/grpc-task-manager/internal/domain/services"
 	"go.uber.org/zap"
 )
 
 type TaskComposite struct {
 	Repository repository.TaskRepository
+	Service    services.TaskService
 }
 
-func NewTaskComposite(repo repository.TaskRepository, logger *zap.Logger) (*TaskComposite, error) {
-	logger.Info("Initializing TaskComposite")
+func NewTaskComposite(db *sql.DB, logger *zap.Logger) (*TaskComposite, error) {
+	taskRepository := storage.NewPostgresTaskRepository(db, logger)
+	taskService := services.NewTaskService(taskRepository, logger)
 	return &TaskComposite{
-		Repository: repo,
+		Repository: taskRepository,
+		Service:    taskService,
 	}, nil
 }
